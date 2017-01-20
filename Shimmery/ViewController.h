9b6c5bca06f0b582de7860f7f8cd3251124c03f5
Mahboud Zabetian <mahboud@optimized.com>
2017-01-19 22:45:13 -0800

    Initial Commit

---
+++ Shimmery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shimmery
+//
+//  Created by mahboud on 1/19/17.
+//  Copyright © 2017 Optimized. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Shimmery/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Shimmery
+//
+//  Created by mahboud on 1/19/17.
+//  Copyright © 2017 Optimized. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
